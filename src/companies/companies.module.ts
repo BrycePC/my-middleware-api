b0d8e1437fb84199cb25a3b5b59a71d9ff401e4e
brycepc <brycepc@hotmail.com>
2026-01-18 12:55:42 +1300

    Fix e2e test post code-tidy

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CompaniesController } from './companies.controller';
-import { CompaniesService } from '../companies.service';
+import { CompaniesService } from './companies.service';
 
 @Module({
   controllers: [CompaniesController],
